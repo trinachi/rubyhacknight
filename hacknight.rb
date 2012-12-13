51ed5d65a25fa6bde132b486716ec343987396dc
Trina Chiasson <trinachi@gmail.com>
2012-12-13 11:15:35 -0600

    give a definition for the first spelling

--- hacknight.rb
+++ hacknight.rb
@@ -1,20 +1,30 @@
 require 'sinatra'
 require 'open-uri'
 require 'nokogiri'
+require 'wordnik'
+require './env'
+
+Wordnik.configure do |config|
+  config.api_key = ENV['wordnik_api_key']
+end
 
 get '/:word' do
-  new_word = Nokogiri::HTML(open("http://spell.ockham.org/?word=#{params[:word]}"))
+  new_word = Nokogiri::HTML(open("http://spell.ockham.org/?word=#{params[:word]}&dictionary=earth"))
+  suggested_spellings = new_word.xpath('//spell/spellings/spelling').children.map(&:to_s)
+  definitions = Wordnik.word.get_definitions(suggested_spellings.first)
   body = ''
   body << "<ul>"
-  new_word.xpath('//spell/spellings/spelling').children.map(&:to_s).each do |word|
+  suggested_spellings.each do |word|
     body << "<li>"
     body << word
     body << "</li>"
   end
-  return body << "</ul>"
+  body << "</ul>"
+  body << "<p>"
+  body << definitions.first["text"].to_s
+  body << "</p>"
 end
 
 get '/' do
   'hello world'
 end
-
